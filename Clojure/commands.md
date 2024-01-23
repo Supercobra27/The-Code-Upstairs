1dcdb343e6aded6d2acdff6a657ddb0a2acd3e0b
Supercobra27 <silverbergryan@gmail.com>
2024-01-23 00:10:34 -0500

    clojure

---
+++ Clojure/commands.md
@@ -0,0 +1,7 @@
+# Commands
+- lein new app (name)
+- lein run
+- lein repl
+
+# Useful Links
+- [https://www.tutorialspoint.com/clojure/clojure_repl.htm|TutorialsPoint]
--- Clojure/tutorial/src/tutorial/core.clj
+++ Clojure/tutorial/src/tutorial/core.clj
@@ -1,7 +1,21 @@
 (ns tutorial.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World! I hate memes"))
+(defn example [] ; Example Function
+  (println (str "Hello Mom!"))
+  (println (* 3 2)) 
+
+  #_{:clj-kondo/ignore [:inline-def]}
+  (def x 1)
+  #_{:clj-kondo/ignore [:inline-def]}
+  (def y 2)
+
+  (println (+ x y))
+  (println  (inc x))
+)
+
+
+(defn -main [] ; Main Function
+  (example)
+  
+  )
