ce423545586e828f61df83eca02c30d229e71228
Supercobra27 <silverbergryan@gmail.com>
2024-02-16 00:05:08 -0500

    update

--- Clojure/tutorial/src/tutorial/core.clj
+++ Clojure/tutorial/src/tutorial/core.clj
@@ -3,26 +3,19 @@
 
 (defn example [] ; Example Function
   (println (str "Hello Mom!"))
-  (println (* 3 2)) 
+  (println (* 3 2))
 
-  #_{:clj-kondo/ignore [:inline-def]}
-  (def x 1)
-  #_{:clj-kondo/ignore [:inline-def]}
-  (def y 2)
-
-  (println (+ x y))
-  (println  (inc x))
-)
+  (let [x 1
+        y 2]
+    (println (+ x y))
+    (println (inc x)))
+  )
 
 (defn arity  ; Loop function
   ([num] (println (+ num 2)))
-  ([] (println (+ 1 2)))
-  )
-
+  ([] (println (+ 1 2))))
 
 (defn -main [] ; Main Function
   (example)
   (arity 6)
-  (arity)
-  
-  )
+  (arity))
