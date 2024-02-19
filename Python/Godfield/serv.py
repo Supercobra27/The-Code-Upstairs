e043a222301850e3f946e1549b7856a29d9cc42b
Supercobra27 <silverbergryan@gmail.com>
2024-02-18 22:44:59 -0500

    add

--- Python/Godfield/serv.py
+++ Python/Godfield/serv.py
@@ -2,6 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+class Card:
+    def __init__(value, element, mana, type):
+            v = value
+            e = element
+            m = mana
+            t = type
+        
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
