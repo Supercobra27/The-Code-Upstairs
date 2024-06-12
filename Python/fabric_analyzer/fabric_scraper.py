56393aaf0537f0821ec056af60119429baa8a8de
Supercobra27 <silverbergryan@gmail.com>
2024-06-12 16:52:52 -0400

    fix

--- Python/fabric_analyzer/fabric_scraper.py
+++ Python/fabric_analyzer/fabric_scraper.py
@@ -1,7 +1,11 @@
-from fabric_functions import run_shell_command, check_directory, create_yt_note
+from fabric_functions import create_yt_note
+from dotenv import load_dotenv
 import pandas as pd
+import os
 
-gfilepath = 'C:\\Users\\Owner\\Documents\\Dev\\Knowledge'
+load_dotenv()
+
+gfilepath = os.getenv('GFILEPATH')
 test = pd.read_csv('test.csv')
 for index, row in test.iterrows():
     create_yt_note(gfilepath, row)
